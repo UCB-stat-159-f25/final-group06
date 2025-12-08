8db3fbf61c473431c7006c9a6cd2e61def82930c
Ethan Briel <e_briel@berkeley.edu>
2025-12-08 22:00:11 +0000

    glaciers package with functions and tests created

---
+++ glaciers/__init__.py
@@ -0,0 +1,7 @@
+"""
+The glaciers package.
+"""
+
+__version__ = "0.0.1"
+
+from .glaciers import *
---
+++ glaciers/tests/test_glaciers.py
@@ -0,0 +1,35 @@
+from glaciers import *
+
+# import all functions to run base tests of outputs
+
+def test_datetime():
+	file_path = glob.glob("data/Karakoram/*_vm_*.tif")
+	assert paths_to_datetimeindex(file_path)[0] == '2021-10-14 - 2021-10-19'
+
+def test_midpoint():
+	geotiff_list = glob.glob("data/Karakoram/*_vm_*.tif")
+
+	# Create variable used for time axis
+	time_var = xr.Variable('time', paths_to_datetimeindex(geotiff_list))
+	
+	# Load in and concatenate all individual GeoTIFFs
+	geotiffs_da = xr.concat(
+	    [rioxarray.open_rasterio(i) for i in geotiff_list],
+	    dim=time_var
+	)
+	
+	# Covert our xarray.DataArray into a xarray.Dataset
+	geotiffs_ds = geotiffs_da.to_dataset('band')
+	
+	# Rename the variable to a more useful name
+	geotiffs_ds = geotiffs_ds.rename({1: 'x_vel', 2: 'y_vel', 3: 'vel_magnitude'})
+	
+	#Adding midpoint time for sorting
+	mid_times = [midpoint(t) for t in geotiffs_ds.time.values]
+	assert mid_times[0].value == 1634385600000000000
+
+def test_to_ds():
+    import xarray as xr
+
+    file_path = "data/Karakoram/*_vm_*.tif"
+    assert isinstance(geotiff_to_ds(file_path), xr.Dataset) == True
